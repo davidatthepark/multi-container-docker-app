5420207b74532b9d9faa9bdbe947075e362eb511
David Park <dpark4657@gmail.com>
2019-09-08 15:51:18 -0700

    initializes complex docker app

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.emitWarning.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+}
