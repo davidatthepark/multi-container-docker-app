af35df0a54e1d814d256ffc0fa88de29d9fede88
David Park <dpark4657@gmail.com>
2019-09-14 11:06:47 -0700

    adds client directory to resolve directory bug

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Fib App!</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage"> Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
